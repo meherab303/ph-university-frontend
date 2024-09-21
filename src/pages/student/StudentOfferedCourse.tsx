12a9ec4c761a4aa7f41e95129c6cd030014d97ee
meherab nahin <nahinrahman87@gmail.com>
2024-09-21 13:35:42 +0600

    student enroll course ui

--- src/pages/student/StudentOfferedCourse.tsx
+++ src/pages/student/StudentOfferedCourse.tsx
@@ -1,18 +1,66 @@
 
+import { Button, Card, Col, Row } from "antd";
 import { useGetAllOfferedCoursesQuery } from "../../redux/features/student/studentCourse.api";
+type TCourse={
+  [index:string]:any
+}
+const gridStyle: React.CSSProperties = {
+  width: '100%',
+  textAlign: 'center',
+  
+};
+const gridStyle2: React.CSSProperties = {
+  width: '25%',
+  textAlign: 'center',
+  display:'flex',
+ alignItems:'center',
+ justifyItems:"center"
+
+};
+const gridStyle3: React.CSSProperties = {
+  width: '12.5%',
+  textAlign: 'center',
+  display:'flex',
+ alignItems:'center',
+ justifyItems:"center"
+ 
+};
 
 const StudentOfferedCourse = () => {
   const {data:offerCourseData}=useGetAllOfferedCoursesQuery(undefined)
-  // console.log(offerCourseData)
-  const modifiedData=offerCourseData?.data?.reduce((acc,item)=>{
+  const modifiedObject=offerCourseData?.data?.reduce((acc:TCourse,item)=>{
     const key=item.course.title
     acc[key]=acc[key] || {courseTitle:key,sections:[]}
-    acc[key].sections.push({section:item.section,_id:item._id,startTime:item.startTime,endTime:item.endTime})
+    acc[key].sections.push({section:item.section,_id:item._id,startTime:item.startTime,endTime:item.endTime,days:item.days})
     return acc
   },{})
-  const finalResult=(Object.values(modifiedData?modifiedData:{}))
+  const finalResult=(Object.values(modifiedObject?modifiedObject:{}))
   console.log(finalResult)
-  return <div>student offered course</div>;
+  return finalResult?.map((item,index)=>{
+    return <>
+    <Card key={index} style={{textAlign:'center'}} title={item.courseTitle}>
+    <Card.Grid style={gridStyle}>{item.sections.map((item)=>{
+      return <>
+     <Row key={item._id}>
+     <Card.Grid style={gridStyle2}> <Col>section: {item.section}</Col></Card.Grid>
+     <Card.Grid style={gridStyle2}> <Col>StartTime: {item.startTime}</Col></Card.Grid>
+     <Card.Grid style={gridStyle2}> <Col>EndTime: {item.endTime}</Col></Card.Grid>
+     <Card.Grid style={gridStyle3}> <Col>{item?.days.map((item:string,idx:number)=>{
+    return(<>
+    <Row key={idx}>
+    {
+      item
+      } 
+    </Row></>)
+     })}</Col></Card.Grid>
+     
+     <Card.Grid onClick={()=>console.log(item._id)}  style={gridStyle3}> <Button>Enroll</Button></Card.Grid>
+     </Row>
+     
+      </>
+    })}</Card.Grid>
+  </Card></>
+  })
 };
 
 export default StudentOfferedCourse;
